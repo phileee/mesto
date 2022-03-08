36c3a5201daddfcdb06695a2a4a5d511b882af1d
Philip Granatuk <phil13@bk.ru>
2022-03-08 11:50:48 +0500

    08.03.22

--- scripts/script.js
+++ scripts/script.js
@@ -2,11 +2,29 @@ let popup = document.querySelector('.popup');
 let openPopup = document.querySelector('.profile__edit-button');
 let closePopup = document.querySelector('.popup__close');
 
+let profileName = document.querySelector('.profile__name');
+let profilePrename = document.querySelector('.profile__prename');
+
+let popupUsername = document.querySelector('.popup__username');
+let popupDescription = document.querySelector('.popup__description');
+
+let formElement = document.querySelector('.popup__form');
+
 openPopup.addEventListener('click', function() {
-  popup.classList.add('popup_opened')
+  popupUsername.value = profileName.textContent;
+  popupDescription.value = profilePrename.textContent;
+  popup.classList.add('popup_opened');
 });
 
 closePopup.addEventListener('click', function() {
-  popup.classList.remove('popup_opened')
+  popup.classList.remove('popup_opened');
 });
 
+function formSubmitHandler (evt) {
+  evt.preventDefault();
+  profileName.textContent = popupUsername.value;
+  profilePrename.textContent = popupDescription.value;
+  popup.classList.remove('popup_opened');
+}
+
+formElement.addEventListener('submit', formSubmitHandler);
